77647d8cf3043708a6845a15dc403748cb53ca20
VincentRouillon <vincent.rouillon@laposte.net>
2018-02-27 15:00:25 +0100

    add projet files

---
+++ src/test/java/PremierTest.java
@@ -0,0 +1,32 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PremierTest {
+    String recherche;
+
+    WebDriver driver;
+
+    @Before
+    public void init(){
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void Test1(){
+        recherche = System.getProperty("recherche");
+        driver.get("http://www.google.com");
+        WebElement search = driver.findElement(By.name("q"));
+        search.sendKeys(recherche);
+        driver.findElement(By.name("btnK")).click();
+    }
+
+    @After
+    public void quit(){
+        driver.quit();
+    }
+}
